766e9e78ed0e3a5284e33cf563b8f285ce054520
elmirgarayev <elmirgarayev96@gmail.com>
2024-06-11 18:40:41 +0400

    lesson3 yerin duzletdim & git ingoner elave etdim lesson3 e

--- Lesson3/Lesson3/obj/Debug/net8.0/Lesson3.AssemblyInfo.cs
+++ Lesson3/Lesson3/obj/Debug/net8.0/Lesson3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+506bd291dc85be74c06c74c7eae4accb37d59a37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d23ed7e39df04fa7d70b4a166c41e85a2fcc3d0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
